2769551e2a45f895e9dfb382154098a3fddb269c
pixelmaid <jennifermaryjacobs@gmail.com>
2026-09-22 15:41:09 -0700

    Deploy site

---
+++ assets/js/5ea8697d.b3886a54.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_site=self.webpackChunkdocusaurus_site||[]).push([["3289"],{8325(e){e.exports=JSON.parse('{"metadata":{"permalink":"/course_site/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.35b4ec20.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus_site=self.webpackChunkdocusaurus_site||[]).push([["3220"],{1912(s){s.exports=JSON.parse('{"blogBasePath":"/course_site/blog","blogTitle":"Blog","authorsListPath":"/course_site/blog/authors"}')}}]);
